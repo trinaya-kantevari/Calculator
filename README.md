ebc7462c5cd8f0b48c594220348b2a367d50ddd9
AlfredoFilho <alfredoalbelisb@gmail.com>
2021-09-30 21:00:47 -0300

    update exampleuse

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Calculator
-Calculator in python for studying unittest
+### Calculator in python for studying unittest
+
+How to use (Example [here](https://github.com/AlfredoFilho/Calculator/blob/main/exampleuse.py)):
+```python
+from Calculator import Calculator
+
+calc = Calculator()
+
+resultDivision = calc.divide(100, 50) # 2.0
+resultMultiplication = calc.multiply(50, 50) # 2500
+resultSum = calc.sum(50, 50) # 100
+resultSubtraction = calc.subtract(50, 50) # 0
+```
+
+
+For run tests:
+```bash
+python test_calculator.py -v
+```
--- exampleuse.py
+++ exampleuse.py
@@ -2,13 +2,13 @@ from Calculator import Calculator
 
 calc = Calculator()
 
-# print(calc.divide(100, 0)) # Raise exception ZeroDivisionError
-# print(calc.divide(50, "50")) # Raise exception TypeError
-# print(calc.multiply(50, "50")) # Raise exception TypeError
-# print(calc.sum(50, "50")) # Raise exception TypeError
-# print(calc.subtract(50, "50")) # Raise exception TypeError
+# resultDivision = calc.divide(100, 0) # Raise exception ZeroDivisionError
+# resultMultiplication = calc.divide(50, "50") # Raise exception TypeError
+# resultSum = calc.multiply(50, "50") # Raise exception TypeError
+# resultSum = calc.sum(50, "50") # Raise exception TypeError
+# resultSubtraction = calc.subtract(50, "50") # Raise exception TypeError
 
-print(calc.divide(100, 50)) # 2.0
-print(calc.multiply(50, 50)) # 2500
-print(calc.sum(50, 50)) # 100
-print(calc.subtract(50, 50)) # 0
+resultDivision = calc.divide(100, 50) # 2.0
+resultMultiplication = calc.multiply(50, 50) # 2500
+resultSum = calc.sum(50, 50) # 100
+resultSubtraction = calc.subtract(50, 50) # 0
